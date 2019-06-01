58f1e1e202c3cbe1d3c59a91c8b2792ba18dea95
崔玉宝 <1176011182@qq.com>
2019-06-01 18:04:32 +0800

    9-4卖家订单controller翻页 9-5卖家订单controller取消订单

--- src/main/java/com/imooc/controller/SellerOrderController.java
+++ src/main/java/com/imooc/controller/SellerOrderController.java
@@ -1,7 +1,10 @@
 package com.imooc.controller;
 
 import com.imooc.dto.OrderDTO;
+import com.imooc.enums.ResultEnum;
+import com.imooc.exception.SellException;
 import com.imooc.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +21,7 @@ import java.util.Map;
  * Date: 2019/5/23
  * Time: 下午 11:22
  */
+@Slf4j
 @Controller
 @RequestMapping("/seller/order")
 public class SellerOrderController {
@@ -39,9 +43,32 @@ public class SellerOrderController {
         PageRequest request=new PageRequest(page-1,size);
         Page<OrderDTO> orderDTOPage= orderService.findList(request);
         map.put("orderDTOPage",orderDTOPage);
+        map.put("currentPage",page);
+        map.put("size",size);
         return new ModelAndView("order/list",map);
-
-
-
     }
+
+    /**
+     * 取消订单
+     * @param orderId
+     * @return
+     */
+    @GetMapping("/cancel")
+    public ModelAndView cancel(@RequestParam("orderId") String orderId,
+    Map<String,Object> map){
+        OrderDTO orderDTO = null;
+        try {
+            orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+        } catch (SellException e) {
+            log.error("[卖家端取消订单]发生异常{}",e);
+            map.put("msg",e.getMessage());
+            map.put("url","/sell/seller/order/list");
+            return new ModelAndView("common/error",map);
+        }
+        map.put("msg",ResultEnum.SUCCESS.getMessage());
+        map.put("url","/sell/seller/order/list");
+      return new ModelAndView("common/success");
+    }
+
 }
